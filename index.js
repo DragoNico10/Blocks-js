8a35030b33ef22929ff34da6696d9463b81591d9
DragoNico10 <arce.nico10@gmail.com>
2022-07-27 18:18:29 -0500

    Co-authored-by: sam1133 <sam1133@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,44 +1,154 @@
-var ball, edges, paddle, canvas
+var ball, edges, paddle, canvas, cpuMode
+var blocksleft=0
+var rows=[]
+var powerupSprites=[]
 var gameState=0
+var speedMultiplier=1
 
 function setup(){
-    canvas=createCanvas(400, 400)
-    ball=createSprite(200, 360,10, 10)
+    cpuMode= createCheckbox('Enable CPU mode', false);
+    canvas=createCanvas(700, 700)
+    ball=createSprite(200, height-40,10, 15)
     ball.draw=()=>{
         push()
         fill(255)
-        ellipse(0, 0, 10)
+        ellipse(0, 0, 15)
         pop()
     }
-    paddle=createSprite(200, 390, 100, 5)
+    paddle=createSprite(200, height, 100, 40)
     paddle.draw=()=>{
         push()
         fill(0, 0, 255)
-        rect(0, 0, 100, 5)
+        rect(0, 0, 100, 40)
         pop()
     }
-    ball.setCollider("circle", 0, 0, 5)
+    ball.setCollider("circle", 0, 0, 20)
     edges=createEdgeSprites()
+    for(let y=100; y<=300; y+=40){
+    rows.push(createBlockSprites(y, 50, 25))
+    }
     
 }
 
 function draw(){
     clear()
     background(75)
+    if(cpuMode.checked()){
+        if(gameState==0){
+            startGame()
+        }
+        if(paddle.x>ball.x){
+            paddle.x-=5*speedMultiplier
+        }
+        if(paddle.x<ball.x){
+            paddle.x+=5*speedMultiplier
+        }
+    }
     if(gameState==0){
         ball.x=paddle.x
     }
-    for(let i=0;i<2;i++){
-        ball.bounceOff(edges[i])
+    if(ball.y>height+10){
+        gameState=2
+        location.reload()
     }
-    if(keyDown(LEFT_ARROW)&&!(paddle.x<55)){
-        paddle.x-=5
+    if(blocksleft===0){
+        location.reload()
     }
-    if(keyDown(RIGHT_ARROW)&&!(paddle.x>345)){
-        paddle.x+=5
+    ball.bounceOff(edges)
+    ball.bounceOff(paddle)
+    for(let ii=0;ii<rows.length;ii++){
+        for(let i=0;i<rows[ii].length;i++){
+            ball.bounceOff(rows[ii][i], ()=>{
+                rows[ii][i].remove()
+                blocksleft--
+            })
+        }
     }
-    if(keyDown(SPACE)&&gameState==0){
-        
+    /*for(let i = 0;i<powerupSprites.length;i++){
+        paddle.collide(powerupSprites[i], ()=>{
+            let sprite = powerupSprites[i]
+            sprite.remove()
+            if(sprite.type==='multiplicate'){
+
+            }
+            if(sprite.type==='launchMore'){
+
+            }
+            powerupSprites.splice(i, 1)
+        })
+    }*/
+    if(keyDown(LEFT_ARROW)&&!(paddle.x<55)&&!(cpuMode.checked())){
+        paddle.x-=5*speedMultiplier
+    }
+    if(keyDown(RIGHT_ARROW)&&!(paddle.x>width-55)&&!(cpuMode.checked())){
+        paddle.x+=5*speedMultiplier
+    }
+    if(keyDown('space')&&gameState==0&&!(cpuMode.checked())){
+        startGame()
     }
     drawSprites()
+}
+let startGame=()=>{
+    gameState=1
+        ball.velocityY=-4*speedMultiplier
+        ball.velocityX=random([-4, 4])*speedMultiplier
+}
+let createBlockSprites=(y, w, h)=>{
+    let r=Math.round(random(0, 255))
+    let g=Math.round(random(0, 255))
+    let b=Math.round(random(0, 255))
+    let row=[]
+    for(let x = 100;x<625;x+=60){
+        let sprite = createSprite(x, y, w, h)
+        blocksleft++
+        sprite.draw=()=>{
+            push()
+            fill(r, g, b)
+            rectMode(CENTER)
+            rect(0, 0, w, h)
+            pop()
+        }
+        row.push(sprite)
+    }
+    return row
+}
+let createPowerupSprite =(type, x, y)=>{
+    let powerupSprite
+    if(type==='multiplicate'){
+        powerupSprite=createSprite(x, y, 40, 40)
+        powerupSprite.draw=()=>{
+            push()
+            fill('blue')
+            rectMode(CENTER)
+            noStroke()
+            rect(0, 0, 40, 40)
+            fill('white')
+            circle(0, -10, 10)
+            circle(10, 10, 10)
+            circle(-10, 10, 10)
+            pop()
+        }
+        powerupsprite.type=type
+        powerupSprite.velocityY=4
+    }
+    if(type==='launchMore'){
+        powerupSprite=createSprite(x, y, 40, 40)
+        powerupSprite.draw=()=>{
+            push()
+            translate(300, 100)
+            rectMode(CENTER)
+            fill('blue')
+            noStroke()
+            rect(0, 0, 40, 40)
+            fill('white')
+            circle(0, 0, 10)
+            circle(13, 0, 10)
+            circle(-13, 0, 10)
+            pop()
+        }
+        powerupSprite.type=type
+        powerupSprite.velocityY=4
+    }
+
+    powerupSprites.push(powerupSprite)
 }
