790ffa6c060e1a8e3d828139996743c3fc4aab44
DragoNico10 <arce.nico10@gmail.com>
2022-08-10 22:34:57 -0500

    Co-authored-by: sam1133 <sam1133@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-var edges, paddle, canvas, cpuMode, rightKey, leftKey, blocksleft=0, rows=[], powerupSprites=[], balls=[], gameState=0, speedMultiplier=1, ballBounceOff, powerupSound, explotionSound
+var edges, paddle, canvas, cpuMode, rightKey, startKey, leftKey, blocksleft=0, rows=[], powerupSprites=[], balls=[], gameState=0, speedMultiplier=1, ballBounceOff, powerupSound, explotionSound
 var gui
 
 let startGame=()=>{
     gameState=1
-        balls[0].velocityY=-4*speedMultiplier*(width/700)
-        balls[0].velocityX=random([-4, 4])*speedMultiplier*(height/700)
+        balls[0].velocityY=-4*speedMultiplier*(height/700)
+        balls[0].velocityX=random([-4*(width/700), 4*(width/700)])*speedMultiplier
 }
 let createBlockSprites=(y, w, h)=>{
     let r=Math.round(random(0, 255))
@@ -62,7 +62,7 @@ let createBall=(x, y, vX, vY, type)=>{
             pop()
         }
     }
-    ball.setCollider('circle', 0, 0, 10)
+    ball.setCollider('circle', 0, 0, (15*width/700)/2)
     ball.velocityX=vX*speedMultiplier
     ball.velocityY=vY*speedMultiplier
     ball.type=type
@@ -70,103 +70,112 @@ let createBall=(x, y, vX, vY, type)=>{
 }
 let createPowerupSprite =(type, x, y, i)=>{
     let powerupSprite
+    let varPriority
+    if(width<height)
+    varPriority=width
+    else
+    varPriority=height
     if(type==='triplicate'){
-        powerupSprite=createSprite(x, y, 40, 40)
+        powerupSprite=createSprite(x, y, 40*(varPriority/700), 40*(varPriority/700))
         powerupSprite.draw=()=>{
             push()
             fill('green')
             rectMode(CENTER)
             noStroke()
-            rect(0, 0, 40, 40)
+            rect(0, 0, 40*(varPriority/700), 40*(varPriority/700))
             fill('white')
-            ellipse(0, -10, 7)
-            ellipse(10, 10, 7)
-            ellipse(-10, 10, 7)
+            ellipse(0, -10*(varPriority/700), 7*(varPriority/700))
+            ellipse(10*(varPriority/700), 10*(varPriority/700), 7*(varPriority/700))
+            ellipse(-10*(varPriority/700), 10*(varPriority/700), 7*(varPriority/700))
             stroke(255)
-            line(4, -5, 9, 8)
-            line(-4, -5, -9, 8)
+            strokeWeight(2*(varPriority/700))
+            line(4*(varPriority/700), -5*(varPriority/700), 9*(varPriority/700), 8*(varPriority/700))
+            line(-4*(varPriority/700), -5*(varPriority/700), -9*(varPriority/700), 8*(varPriority/700))
             pop()
         }
         powerupSprite.balli=i
     }
     if(type==='launchMore'){
-        powerupSprite=createSprite(x, y, 40, 40)
+        powerupSprite=createSprite(x, y, 40*(varPriority/700), 40*(varPriority/700))
         powerupSprite.draw=()=>{
             push()
             rectMode(CENTER)
             fill('green')
             noStroke()
-            rect(0, 0, 40, 40)
+            rect(0, 0, 40*(varPriority/700), 40*(varPriority/700))
             fill('white')
-            ellipse(10, -10, 7)
-            ellipse(-10, -10, 7)
-            rect(0, 10, 25, 5)
+            ellipse(10*(varPriority/700), -10*(varPriority/700), 7*(varPriority/700))
+            ellipse(-10*(varPriority/700), -10*(varPriority/700), 7*(varPriority/700))
+            rect(0*(varPriority/700), 10*(varPriority/700), 25*(varPriority/700), 5*(varPriority/700))
             stroke(255)
-            line(10, -10, 3, 5)
-            line(-10, -10, -3, 5)
+            strokeWeight(2*(varPriority/700))
+            line(10*(varPriority/700), -10*(varPriority/700), 3*(varPriority/700), 5*(varPriority/700))
+            line(-10*(varPriority/700), -10*(varPriority/700), -3*(varPriority/700), 5*(varPriority/700))
             pop()
         }
     }
     if(type==='explotionBall'){
-        powerupSprite=createSprite(x, y, 40, 40)
+        powerupSprite=createSprite(x, y, 40*(varPriority/700), 40*(varPriority/700))
         powerupSprite.draw=()=>{
             push()
             rectMode(CENTER)
             fill('cyan')
             noStroke()
-            rect(0, 0, 40, 40)
+            rect(0, 0, 40*(varPriority/700), 40*(varPriority/700))
             fill('white')
-            triangle(5, 0, 0, -20, -5, 0)
-            triangle(0, 0, 10, -15, -5, -5)
-            triangle(0, 3, 0, -7, 15, -5)
-            triangle(15, 15, 0, -15, -5, 0)
-            triangle(5, 0, 0, 15, -5, 0)
-            triangle(5, 0, -10, -15, -5, 0)
-            triangle(15, 15, 0, -15, -5, 0)
-            triangle(0, 3, 0, -7, -15, 3)
+            triangle(5*(varPriority/700), 0, 0, -20*(varPriority/700), -5*(varPriority/700), 0)
+            triangle(0, 0, 10*(varPriority/700), -15*(varPriority/700), -5*(varPriority/700), -5*(varPriority/700))
+            triangle(0, 3*(varPriority/700), 0, -7*(varPriority/700), 15*(varPriority/700), -5*(varPriority/700))
+            triangle(15*(varPriority/700), 15*(varPriority/700), 0, -15*(varPriority/700), -5*(varPriority/700), 0)
+            triangle(5*(varPriority/700), 0, 0, 15*(varPriority/700), -5*(varPriority/700), 0)
+            triangle(5*(varPriority/700), 0, -10*(varPriority/700), -15*(varPriority/700), -5*(varPriority/700), 0)
+            triangle(15*(varPriority/700), 15*(varPriority/700), 0, -15*(varPriority/700), -5*(varPriority/700), 0)
+            triangle(0, 3*(varPriority/700), 0, -7*(varPriority/700), -15*(varPriority/700), 3*(varPriority/700))
             fill('orange')
-            ellipse(0.5, -2.5, 7)
+            ellipse(0.5*(varPriority/700), -2.5*(varPriority/700), 7*(varPriority/700))
             pop()
         }
     }
     if(type==='lengthenPaddle'){
-        powerupSprite=createSprite(x, y, 40, 40)
+        powerupSprite=createSprite(x, y, 40*(varPriority/700), 40*(varPriority/700))
         powerupSprite.draw=()=>{
             push()
             rectMode(CENTER)
             fill('blue')
             rectMode(CENTER)
             noStroke()
-            rect(0, 0, 40)
+            rect(0, 0, 40*(varPriority/700))
             fill('white')
-            rect(0, 0, 15, 5)
+            rect(0, 0, 15*(varPriority/700), 5*(varPriority/700))
             stroke(255)
-            line(10, 0, 15, 0)
-            line(15, 0, 12, -3)
-            line(15, 0, 12, 3)
-            line(-10, 0, -15, 0)
-            line(-15, 0, -12, -3)
-            line(-15, 0, -12, 3)
+            strokeWeight(2*(varPriority/700))
+            line(10*(varPriority/700), 0, 15*(varPriority/700), 0)
+            line(15*(varPriority/700), 0, 12*(varPriority/700), -3*(varPriority/700))
+            line(15*(varPriority/700), 0, 12*(varPriority/700), 3*(varPriority/700))
+            line(-10*(varPriority/700), 0, -15*(varPriority/700), 0)
+            line(-15*(varPriority/700), 0, -12*(varPriority/700), -3*(varPriority/700))
+            line(-15*(varPriority/700), 0, -12*(varPriority/700), 3*(varPriority/700))
             pop()
         }
     }
     if(type==='shortenPaddle'){
-        powerupSprite=createSprite(x, y, 40, 40)
+        powerupSprite=createSprite(x, y, 40*(varPriority/700), 40*(varPriority/700))
         powerupSprite.draw=()=>{
             push()
             fill('red')
             rectMode(CENTER)
             noStroke()
-            rect(0, 0, 40)
+            rect(0, 0, 40*(varPriority/700))
             fill('white')
-            rect(0, 0, 15, 5)
+            rect(0, 0, 15*(varPriority/700), 5*(varPriority/700))
             stroke(255)
-            line(10, 0, 15, 0)
-            line(10, 0, 13, -3)
-            line(10, 0, 13, 3)
-            line(-10, 0, -15, 0)
-            line(-10, 0, -13, -3)
-            line(-10, 0, -13, 3)
+            strokeWeight(2*(varPriority/700))
+            line(10*(varPriority/700), 0, 15*(varPriority/700), 0)
+            line(10*(varPriority/700), 0, 13*(varPriority/700), -3*(varPriority/700))
+            line(10*(varPriority/700), 0, 13*(varPriority/700), 3*(varPriority/700))
+            line(-10*(varPriority/700), 0, -15*(varPriority/700), 0)
+            line(-10*(varPriority/700), 0, -13*(varPriority/700), -3*(varPriority/700))
+            line(-10*(varPriority/700), 0, -13*(varPriority/700), 3*(varPriority/700))
             pop()
         }
     }
@@ -178,12 +187,12 @@ let createPowerupSprite =(type, x, y, i)=>{
             fill('gold')
             rectMode(CENTER)
             noStroke()
-            rect(0, 0, 40)
+            rect(0, 0, 40*(varPriority/700))
             fill('white')
-            rect(-3, 3, 50/3, 25/3)
-            let r1=2
-            let r2=10
-            translate(-20, -20)
+            rect(-3*(varPriority/700), 3*(varPriority/700), 50/3*(varPriority/700), 25/3*(varPriority/700))
+            let r1=2*(varPriority/700)
+            let r2=10*(varPriority/700)
+            translate(-20*(varPriority/700), -20*(varPriority/700))
             beginShape();
             vertex(0-r1, 0-r1);
             vertex(0, 0-r2)
@@ -194,7 +203,7 @@ let createPowerupSprite =(type, x, y, i)=>{
             vertex(0-r1, r1)
             vertex(0-r2, 0)
             endShape();
-            translate(40, 40)
+            translate(40*(varPriority/700), 40*(varPriority/700))
             beginShape();
             vertex(0-r1, 0-r1);
             vertex(0, 0-r2)
@@ -205,20 +214,20 @@ let createPowerupSprite =(type, x, y, i)=>{
             vertex(0-r1, r1)
             vertex(0-r2, 0)
             endShape();
-            translate(-20,-20)
+            translate(-20*(varPriority/700),-20*(varPriority/700))
             fill('red')
-            ellipse(3, -3, 15)
+            ellipse(3*(varPriority/700), -3*(varPriority/700), 15*(varPriority/700))
             fill('gold')
-            ellipse(3, -3, 10)
+            ellipse(3*(varPriority/700), -3*(varPriority/700), 10*(varPriority/700))
             stroke('red')
-            strokeWeight(3)
-            line(0, -8.5, 6, 2.5)
+            strokeWeight(3*(varPriority/700))
+            line(0, -8.5*(varPriority/700), 6*(varPriority/700), 2.5*(varPriority/700))
             pop()
         }
     }
     if(powerupSprite){
         powerupSprite.type=type
-        powerupSprite.velocityY=4*speedMultiplier
+        powerupSprite.velocityY=(4*speedMultiplier)*(height/700)
         powerupSprites.push(powerupSprite)
     }
 }
@@ -252,13 +261,23 @@ function setup(){
     canvas=createCanvas(windowWidth, windowHeight)
     gui=createGui()
     cpuMode= createCheckbox('cpu mode', 0, 0, 20*(width/700), 20*(width/700));
-    paddle=createSprite(width/2, height, 100*(width/700), 40*(height/700))
+    paddle=createSprite(width/2, height, 100*(width/500), 40*(height/1000))
     balls.push(createBall(paddle.x, height-(height/20), 0, 0, 'normal'))
     paddle.draw=()=>{}
     edges=createEdgeSprites()
     for(let y=height/25; y<=height/2-(height/100); y+=height/20){
-    rows.push(createBlockSprites(y, 50*(height/700), 25*(height/700)))
+        rows.push(createBlockSprites(y, 50*(width/1500), 25*(height/700)))
     }
+    leftKey=createButton("<", width/200, height-(height/100)-(50*height/700), 50*height/700, 50*height/700)
+    rightKey=createButton(">", width-(width/200)-(50*height/700), height-(height/100)-(50*height/700), 50*height/700, 50*height/700)
+    startKey=createButton('Start', width/4, height-(height/6), width/2, height/10)
+    leftKey._style.textSize=20*width/700
+    rightKey._style.textSize=20*width/700
+    startKey._style.textSize=20*width/700
+    startKey._style.strokeWeight=2*width/700
+    rightKey._style.strokeWeight=2*width/700
+    leftKey._style.strokeWeight=2*width/700
+    cpuMode._style.strokeWeight=2*width/700
 }
     
     
@@ -327,13 +346,13 @@ function draw(){
                             explotionSound.play()
                             ball.velocityX=0
                             ball.velocityY=0
-                            ball.width=100
-                            ball.height=100
-                            ball.setCollider('circle', 0, 0, 50)
+                            ball.width=100*(width/700)
+                            ball.height=100*(width/700)
+                            ball.setCollider('circle', 0, 0, 50*(width/700))
                             ball.draw=()=>{
                                 push()
                                 fill('orange')
-                                ellipse(0, 0, 100)
+                                ellipse(0, 0, 100*(width/700))
                                 pop()
                             }
                             for(let i=0;i<rows.length;i++){
@@ -375,23 +394,23 @@ function draw(){
                         while(!(balls[sprite.balli])){
                             sprite.balli--
                         }
-                        balls.push(createBall(balls[sprite.balli].x, balls[sprite.balli].y,4, -4, 'normal'))
-                        balls.push(createBall(balls[sprite.balli].x, balls[sprite.balli].y, -4, -4, 'normal'))
+                        balls.push(createBall(balls[sprite.balli].x, balls[sprite.balli].y,4*(width/700)*speedMultiplier, -4*(height/700)*speedMultiplier, 'normal'))
+                        balls.push(createBall(balls[sprite.balli].x, balls[sprite.balli].y, -4*(width/700)*speedMultiplier, -4*(height/700)*speedMultiplier, 'normal'))
                     }
                     if(sprite.type==='launchMore'){
-                        balls.push(createBall(paddle.x, paddle.y-20, random(-4, 4), 4,'normal'))
-                        balls.push(createBall(paddle.x, paddle.y-20, random(-4, 4), 4, 'normal'))
+                        balls.push(createBall(paddle.x, paddle.y-20, random(-4*(width/700)*speedMultiplier, 4*(width/700)), 4*(height/700)*speedMultiplier,'normal'))
+                        balls.push(createBall(paddle.x, paddle.y-20, random(-4*(width/700)*speedMultiplier, 4*(width/700)), 4*(height/700)*speedMultiplier, 'normal'))
                     }
                     if(sprite.type==='explotionBall'){
-                        balls.push(createBall(paddle.x, paddle.y-20, 0, -4, 'explosive'))
+                        balls.push(createBall(paddle.x, paddle.y-20, 0, -4*(height/700), 'explosive'))
                     }
                     if(sprite.type==='lengthenPaddle'){
-                        paddle.width+=10
-                        setTimeout(()=>{paddle.width-=10}, 10000)
+                        paddle.width+=10*(width/700)
+                        setTimeout(()=>{paddle.width-=10*(width/700)}, 10000/speedMultiplier)
                     }
                     if(sprite.type==='shortenPaddle'){
-                        paddle.width-=10
-                        setTimeout(()=>{paddle.width+=10}, 10000)
+                        paddle.width-=10*(width/700)
+                        setTimeout(()=>{paddle.width+=10*(width/700)}, 10000/speedMultiplier)
                     }
                     if(sprite.type==='finishLevel'){
                         for(let e = 0;e<rows.length;e++){
@@ -418,6 +437,31 @@ function draw(){
         if(keyDown('space')&&gameState==0&&!(cpuMode.val)){
             startGame()
         }
+        
+        leftKey.enabled=false
+        leftKey.visible=false
+        rightKey.enabled=false
+        rightKey.visible=false
+    }
+    if(getDeviceType()=='tablet'||getDeviceType()=='mobile'){
+        if(leftKey.val&&!(paddle.x<0)&&!(cpuMode.val)){
+            paddle.x-=5*speedMultiplier*(width/700)
+        }
+        if(rightKey.val&&!(paddle.x>width)&&!(cpuMode.val)){
+            paddle.x+=5*speedMultiplier*(width/700)
+        }
+        if(startKey.val&&gameState==0&&!(cpuMode.val)){
+            startGame()
+        }
+        if(gameState===1){
+            startKey.enabled=false;startKey.visible=false}
+        
+        if(gameState===0){
+            startKey.enabled=true;startKey.visible=true}
+        leftKey.enabled=true
+        leftKey.visible=true
+        rightKey.enabled=true
+        rightKey.visible=true
     }
     drawSprites()
     drawPaddle()
@@ -426,18 +470,33 @@ function draw(){
     textAlign(LEFT)
     fill(255)
     textSize(width/100)
-    text('Enable CPU mode', cpuMode.w+(cpuMode.w/10), cpuMode.w/2)
+    text('Enable CPU mode', cpuMode.w+(cpuMode.w/10), (cpuMode.w/2)+(cpuMode.w/10))
     pop()
-    if(gameState==0){
+    if(gameState==0&&getDeviceType()=='desktop'){
         balls[0].x=paddle.x
         textAlign(CENTER)
         push()
         noStroke()
         textSize(width/100)
         fill(255)
-        text('To start press space and move width the right and arrow keys,', width/2, height/2-(height/100))
+        text('To start press space and move with the right and left arrow keys,', width/2, height/2-(height/100))
         text('or just enable CPU mode', width/2, height/2+(height/100))
         pop()
+        startKey.enabled=false
+        startKey.visible=false
+    }
+    if(gameState==0&&(getDeviceType()=='tablet'||getDeviceType()=='mobile')){
+        balls[0].x=paddle.x
+        textAlign(CENTER)
+        push()
+        noStroke()
+        textSize(width/100)
+        fill(255)
+        text('To start press the start button and move with the right and left buttons,', width/2, height/2-(height/100))
+        text('or just enable CPU mode', width/2, height/2+(height/100))
+        pop()
+        startKey.enabled=true
+        startKey.visible=true
     }
 }
 let selectPowerup=()=>{
@@ -448,14 +507,14 @@ let selectPowerup=()=>{
     else if(select>75){
         return 'launchMore'
     }
-    else if(select>45){
+    else if(select>55){
         return 'shortenPaddle'
+    }
+    else if(select>35){
+        return 'lengthenPaddle'
     }
     else if(select>20){
         return 'explotionBall'
-    }
-    else if(select>20){
-        return 'lengthenPaddle'
     }
     else if(select<20){
         return 'triplicate'
